113ce343b91c4f9b6d62dd5f81d28c227396be52
Porridge34 <ivy.yan.ng@gmail.com>
2021-12-14 21:08:04 -0500

    bruh

--- navbar.js
+++ navbar.js
@@ -9,62 +9,114 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var NavBar = function (_React$Component) {
-		_inherits(NavBar, _React$Component);
+	_inherits(NavBar, _React$Component);
 
-		function NavBar() {
-				_classCallCheck(this, NavBar);
+	function NavBar() {
+		_classCallCheck(this, NavBar);
 
-				return _possibleConstructorReturn(this, (NavBar.__proto__ || Object.getPrototypeOf(NavBar)).apply(this, arguments));
-		}
+		return _possibleConstructorReturn(this, (NavBar.__proto__ || Object.getPrototypeOf(NavBar)).apply(this, arguments));
+	}
 
-		_createClass(NavBar, [{
-				key: 'render',
-				value: function render() {
-						return React.createElement(
-								'h1',
-								null,
-								'test'
+	_createClass(NavBar, [{
+		key: "render",
+		value: function render() {
+			return (
+				//   <h1>test</h1>
+				React.createElement(
+					"div",
+					{ "class": "nav-bar" },
+					React.createElement("link", { rel: "stylesheet", href: "bootstrap.min.css" }),
+					React.createElement(
+						"nav",
+						{ "class": "navbar navbar-expand-lg navbar-dark bg-primary" },
+						React.createElement(
+							"div",
+							{ "class": "container-fluid" },
+							React.createElement(
+								"a",
+								{ "class": "navbar-brand", href: "#" },
+								"CSimplified"
+							),
+							React.createElement(
+								"button",
+								{ "class": "navbar-toggler", type: "button", "data-bs-toggle": "collapse", "data-bs-target": "#navbarColor01", "aria-controls": "navbarColor01", "aria-expanded": "false", "aria-label": "Toggle navigation" },
+								React.createElement("span", { "class": "navbar-toggler-icon" })
+							),
+							React.createElement(
+								"div",
+								{ "class": "collapse navbar-collapse", id: "navbarColor01" },
+								React.createElement(
+									"ul",
+									{ "class": "navbar-nav me-auto" },
+									React.createElement(
+										"li",
+										{ "class": "nav-item" },
+										React.createElement(
+											"a",
+											{ "class": "nav-link", href: "index.html" },
+											"Home"
+										)
+									),
+									React.createElement(
+										"li",
+										{ "class": "nav-item" },
+										React.createElement(
+											"a",
+											{ "class": "nav-link", href: "mission.html" },
+											"Mission"
+										)
+									),
+									React.createElement(
+										"li",
+										{ "class": "nav-item" },
+										React.createElement(
+											"a",
+											{ "class": "nav-link", href: "getcsimplified.html" },
+											"Get CSimplified"
+										)
+									),
+									React.createElement(
+										"li",
+										{ "class": "nav-item" },
+										React.createElement(
+											"a",
+											{ "class": "nav-link ", href: "about.html" },
+											"Reviews"
+										)
+									),
+									React.createElement(
+										"li",
+										{ "class": "nav-item dropdown" },
+										React.createElement(
+											"a",
+											{ "class": "nav-link dropdown-toggle", "data-bs-toggle": "dropdown", role: "button", "aria-haspopup": "true", "aria-expanded": "false" },
+											"About"
+										),
+										React.createElement(
+											"div",
+											{ "class": "dropdown-menu" },
+											React.createElement(
+												"a",
+												{ "class": "dropdown-item", href: "socials.html" },
+												"Our Socials"
+											),
+											React.createElement(
+												"a",
+												{ "class": "dropdown-item", href: "contactus.html" },
+												"Contact Us"
+											)
+										)
+									)
+								)
+							)
 						)
-						// <div class = "nav-bar">
-						// 	<link rel="stylesheet" href="bootstrap.min.css" />
-						// 	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-						// 	<div class="container-fluid">
-						// 	  <a class="navbar-brand" href="#">CSimplified</a>
-						// 	  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-						// 		<span class="navbar-toggler-icon"></span>
-						// 	  </button>
+					)
+				)
+			);
+		}
+	}]);
 
-						// 	  <div class="collapse navbar-collapse" id="navbarColor01">
-						// 		<ul class="navbar-nav me-auto">
-						// 		  <li class="nav-item">
-						// 			<a class="nav-link" href="index.html">Home</a>
-						// 		  </li>
-						// 		  <li class="nav-item">
-						// 			<a class="nav-link" href="mission.html">Mission</a>
-						// 		  </li>
-						// 		  <li class="nav-item">
-						// 			<a class="nav-link active" href="getcsimplified.html">Get CSimplified</a>
-						// 		  </li>
-						// 		  <li class="nav-item">
-						// 			<a class="nav-link " href="about.html">Reviews</a>
-						// 		  </li>
-						// 		  <li class="nav-item dropdown">
-						// 			<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About</a>
-						// 			<div class="dropdown-menu">
-						// 			  <a class="dropdown-item" href="socials.html">Our Socials</a>
-						// 			  <a class="dropdown-item" href="contactus.html">Contact Us</a>
-						// 			</div>
-						// 		  </li>
-						// 		</ul>
-						// 	  </div>
-						// 	</div>
-						//   </nav>
-						// </div>
-						;
-				}
-		}]);
-
-		return NavBar;
+	return NavBar;
 }(React.Component);
 
 var domContainer = document.querySelector('#nav-bar');
